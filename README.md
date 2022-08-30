b764d696df25d4ab7da77b79fc96d695458e973c
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-08-30 16:27:08 -0500

    increment

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Basic Redux counter minus the tool kit.
 ## npm create-react-app my-app
 
 ## npm install redux react-redux
+
+## npm start
--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,7 +1,11 @@
 import {createStore} from 'redux';
 
 const reducerFn = (state ={counter : 0}, action) => {
-     return state;
+    if (action.type === "INC"){
+      return  {counter: state.counter + 1};
+    }
+    
+    return state;
 }
 const store = createStore(reducerFn)
 
